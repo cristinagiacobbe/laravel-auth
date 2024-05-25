5411e1af5b28a23f0717382bd0e0dde57c93d7e4
cristinagiacobbe <crigiak@gmail.com>
2024-05-25 22:34:56 +0200

    Create relation one to many type->projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'description', 'cover_image', 'project_url', 'slug'];
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
